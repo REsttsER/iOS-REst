764af4cb2c3aafc11f2702d29aeb10d5ef0af516
jangsilverbaby <dmsdo1234@gmail.com>
2022-06-20 16:32:40 +0900

    [feat] Coordinator 패턴

---
+++ REst/Presentation/Common/Coordinator/Protocol/AppCoordinator.swift
@@ -0,0 +1,13 @@
+//
+//  AppCordinator.swift
+//  REst
+//
+//  Created by 장은애(Eunae Jang) on 2022/06/20.
+//
+
+import Foundation
+
+protocol AppCoordinator: Coordinator {
+    func showLoginFlow()
+    func showTabBarFlow()
+}
