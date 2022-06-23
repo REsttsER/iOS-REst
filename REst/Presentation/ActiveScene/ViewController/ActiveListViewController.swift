1ea4cb3ec4cb4b3078bc800a2ac57e6bb8e2e377
jangsilverbaby <dmsdo1234@gmail.com>
2022-06-23 16:49:44 +0900

    [feat] configureUI() 추가

--- REst/Presentation/ActiveScene/ViewController/ActiveListViewController.swift
+++ REst/Presentation/ActiveScene/ViewController/ActiveListViewController.swift
@@ -33,6 +33,8 @@ final class ActiveListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+        bindViewModel()
     }
 }
 
