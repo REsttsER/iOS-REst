1355fb7701941f9dc0e0c367f2195e00e203cc19
jangsilverbaby <dmsdo1234@gmail.com>
2022-06-23 16:45:43 +0900

    [feat] Active Model 추가

---
+++ REst/Domain/Entities/Active.swift
@@ -0,0 +1,15 @@
+//
+//  Active.swift
+//  REst
+//
+//  Created by 장은애(Eunae Jang) on 2022/06/23.
+//
+
+import Foundation
+
+struct Active: Equatable/*, Identifiable*/ {
+    //typealias Identifier = String
+    //let id: ObjectIdentifier
+    let i: Int
+    let date: Date
+}
